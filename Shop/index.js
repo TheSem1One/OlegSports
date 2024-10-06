8d9cd40c674ff0549b2a5c84af9ea5006407f98b
TheSem1One <animehub021@gmail.com>
2024-10-06 18:50:38 +0300

    Added auth and some library

--- Shop/index.js
+++ Shop/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from 'mongoose';
-
+import { registerValidation } from './validations/auth.js';
+import { validationResult } from "express-validator";
+import UserModel from './models/user.js';
+import { use } from 'bcrypt/promises.js';
 
 mongoose.connect(
-    'mongodb+srv://admin:admin@cluster0.apc7q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    'mongodb+srv://admin:admin@cluster0.apc7q.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
     .then(()=>console.log('DB ok '))
     .catch((err) => console.log ('DB error', err));
 
@@ -18,9 +22,96 @@ app.get('/' , (req, res) =>{
     res.send('111 Hello World!');
 })
 
-app.post('/auth/register' , (req,res)=>{
-   
+app.post('/auth/login' ,async (req,res) =>{
+    try{
+        const user = await UserModel.findOne({ email : req.body.email })
+        if(!user){
+            return res.status(404).json({
+                message : 'Невірний логін або пароль',
+            });
+        }
+
+
+    const isValidPass =await bcrypt.compare(req.body.password , user._doc.passwordHash);
+    if (!isValidPass){
+        return res.status(404).json({
+            message :'Невірний логін або пароль',
+        });
+    }
+
+    const token =jwt.sign({
+        _id : user._id,
+    }, 
+    'uzhnu2024',
+    {
+        expiresIn: '30d'
+    });
+
+
+    const {passwordHash, ...userData}= user._doc;
+    res.json({
+        ...userData,
+        token});
+
+    }
+    catch(err){
+        console.log (err);
+        res.status(500).json({
+            message : "Не вдалось авторизуватись",
+        });
+    }
 })
+app.post('/auth/register' ,registerValidation,async (req,res)=>{
+   try{
+        const errors  =  validationResult(req);
+        if (!errors.isEmpty()){
+        return res.status(400).json(errors.array());
+    }
+
+
+        const password =req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password , salt);
+
+
+        const doc = new UserModel({
+        email : req.body.email,
+        firstName: req.body.firstName,
+        lastName : req.body.lastName,
+        passwordHash :hash,
+        avatarUrl : req.body.avatarUrl
+
+    });
+
+    const user = await doc.save();
+    const token =jwt.sign({
+        _id : user._id,
+    }, 
+    'uzhnu2024',
+    {
+        expiresIn: '30d'
+    });
+
+    const {passwordHash, ...userData}= user._doc;
+    res.json({
+        ...userData,
+        token});
+   }
+   catch(err){
+    console.log (err);
+    res.status(500).json({
+        message : "can't create a register"
+    })}
+});
+
+app.get('/profile' , (req,res)=>{
+    try{
+        
+    }
+    catch(err){
+
+    }
+});
 
 app.listen(4444, (err) => {
     if (err){
--- Shop/validations/auth.js
+++ Shop/validations/auth.js
@@ -1,5 +1,8 @@
 import { body } from "express-validator";
-const registerValidator = [
-    body('email').isEmail(),
-    body('password').isLength({min : 8}),
+export const registerValidation = [
+    body('email','Неправильний логін чи пароль').isEmail(),
+    body('password','Неправильний логін чи пароль').isLength({min : 8}),
+    body('firstName','Вкажіть імя').isLength({min : 3}),
+    body('lastName' , 'Вкажіть імя').isLength({min : 4}),
+    body('avatarUrl').optional().isURL(),
 ];
